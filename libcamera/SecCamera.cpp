180b814b9456832b91e9e37bc0ae4ee6c6db716c
adis1313 <adis1313@ubuntu.(none)>
2013-09-05 19:00:17 +0200

    Sources adjusted to ICS

--- libcamera/SecCamera.cpp
+++ libcamera/SecCamera.cpp
@@ -477,7 +477,7 @@ static int fimc_v4l2_s_ext_ctrl(int fp, unsigned int id, void *value)
     int ret;
 
     ctrl.id = id;
-    ctrl.string = (char *) value;
+    ctrl.string = value;
 
     ctrls.ctrl_class = V4L2_CTRL_CLASS_CAMERA;
     ctrls.count = 1;
